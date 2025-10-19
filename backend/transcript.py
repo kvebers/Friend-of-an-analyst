cd48ff838fb8ffa38875f7ea2c13707faa240cf0
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2025-10-19 12:06:42 +0300

    Fix api;

--- backend/transcript.py
+++ backend/transcript.py
@@ -7,8 +7,8 @@ def get_transcript_from_id(video_id):
     """ 
     ytt_api = YouTubeTranscriptApi() 
     content = ytt_api.fetch(video_id) 
-    print(content)
-    transcript_text = ' '.join([segment['text'] for segment in content])
+
+    transcript_text = ' '.join([segment.text for segment in content])
     return transcript_text
 
         
